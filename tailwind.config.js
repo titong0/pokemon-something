c1fe73ec9c93e0f5d119cb2a20e3cc8e2851fee8
titong0 <augustoerrecarte@gmail.com>
2021-10-07 19:03:12 -0300

    this definitely fixes it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     options: {
-      safelist: ["bg-pink-300", "bg-red-300"],
+      safelist: [
+        "bg-pink-300",
+        "bg-blue-300",
+        "bg-yellow-300",
+        "bg-gray-300",
+        "bg-red-300",
+      ],
     },
   },
-  darkMode: false, 
+  darkMode: false,
   theme: {
     extend: {},
   },
